7dba45e4fd2e892888ab819a571dfe74165f827b
Icadar22 <cadariuliana2000@yahoo.com>
2022-12-14 19:53:04 +0200

    LAB9

--- ListEntryPage.xaml.cs
+++ ListEntryPage.xaml.cs
@@ -30,4 +30,13 @@ public partial class ListEntryPage : ContentPage
             });
         }
     }
+    async void OnChooseButtonClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new ProductPage((ShopList)
+       this.BindingContext)
+        {
+            BindingContext = new Product()
+        });
+
+    }
 }
